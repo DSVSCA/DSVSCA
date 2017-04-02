7a9e4d817c6ea510015fd94259e207ed964220f4
Conor Landry <clandry94@ufl.edu>
2017-04-02 18:10:50 -0400

    Refactor encoder into its own class

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,34 @@
 #include <iostream>
 #include "input/filter.h"
 #include "input/format.h"
+#include "encoder/encoder.h"
 #include "sjoin/sjoin.h"
 #include <ctime>
 #include "mysofa.h"
 #include <stdio.h>
-
-int check_sample_fmt(AVCodec *codec, enum AVSampleFormat sample_fmt) {
-    const enum AVSampleFormat *p = codec->sample_fmts;
-    while(*p != AV_SAMPLE_FMT_NONE) {
-        if(*p == sample_fmt)
-            return 1;
-        p++;
-    }
-    return 0;
-}
-
-int select_sample_rate(AVCodec *codec) {
-    const int *p;
-    int best_samplerate = 0;
-
-    if(!codec->supported_samplerates)
-        return 44100;
-
-    p = codec->supported_samplerates;
-    while(*p) {
-        best_samplerate = FFMAX(*p, best_samplerate);
-        p++;
-    }
-    return best_samplerate;
-
-}
 
 
 int process_filter_graph(Format *fmt, Filter *filter, SJoin *sj) {
     
     FILE *f;
     const char *filename = "FL.aac";
-
-    AVPacket pkt;
-    int got_output;
     
     AVPacket packet, packet0;
     AVFrame *frame = av_frame_alloc();
     AVFrame *filt_frame = av_frame_alloc();
+    int got_frame;
 
-    AVCodec *codec;
-    AVCodecContext *c = NULL;
+    AVPacket packet_out;
+    int got_output;
 
-    codec = avcodec_find_encoder(AV_CODEC_ID_AC3);
-    if(!codec) {
-        std::cout << "Codec not found!" << std::endl;
-        exit(1);
-    }
-
-    c = avcodec_alloc_context3(codec);
-    if(!c) {
-        std::cout << "Failed to allocate audio codec context" << std::endl;
-    }
-
-    int buffer_size;
-
-    c->bit_rate = fmt->decoder_ctx->bit_rate;
-    c->sample_fmt = AV_SAMPLE_FMT_FLTP;
-    if(!check_sample_fmt(codec, c->sample_fmt)) {
-        std::cout << "Does not support sample format" << std::endl;
-        exit(1);
-    }
-
-    c->sample_rate = select_sample_rate(codec);
-    c->channel_layout = 3;
-    c->channels = 2;
-
-    if(avcodec_open2(c, codec, NULL) < 0) {
-        std::cout << "Could not open codec" << std::endl;
-        exit(1);
-    }
+    Encoder *encoder = new Encoder(AV_CODEC_ID_AC3,
+            fmt->decoder_ctx->bit_rate, AV_SAMPLE_FMT_FLTP);
 
     f = fopen(filename, "wb");
     if(!f) {
         std::cout << "Error opening file" << std::endl;
     }
-
-    buffer_size = av_samples_get_buffer_size(NULL, c->channels, c->frame_size, c->sample_fmt, 0);
-
-    if(buffer_size < 0) {
-        std::cout << buffer_size << " COULD NOT GET SAMPLE BUFFER SIZE" << std::endl;
-        exit(1);
-    }
-
-    av_malloc(buffer_size);
-    std::cout << "BUFFER SIZE: " << buffer_size << std::endl;
-
-
-    int got_frame;
+    
     int ret = 0;
 
     /* Read all of the packets */
@@ -136,15 +71,15 @@ int process_filter_graph(Format *fmt, Filter *filter, SJoin *sj) {
                     if(ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) break;
                     //std::cout << "FR";
                     
-                    av_init_packet(&pkt);
-                    pkt.data = NULL;
-                    pkt.size = 0;
+                    av_init_packet(&packet_out);
+                    packet_out.data = NULL;
+                    packet_out.size = 0;
                     
-                    ret = avcodec_encode_audio2(c, &pkt, filt_frame, &got_output);
+                    ret = avcodec_encode_audio2(encoder->codec_ctx, &packet_out, filt_frame, &got_output);
                     if(ret < 0) exit(1);
                     if(got_output) {
-                        fwrite(pkt.data, 1, pkt.size, f);
-                        av_free_packet(&pkt);
+                        fwrite(packet_out.data, 1, packet_out.size, f);
+                        av_free_packet(&packet_out);
                     }    
                                       
                     av_frame_unref(filt_frame);
