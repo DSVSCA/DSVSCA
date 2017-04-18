6635627a4a491f3d7a46ce0151660a1d1db4ab1c
Your Name <(none)>
2017-04-18 18:10:19 -0400

    Handle opening file in current directory

--- mpv/auto_convert.lua
+++ mpv/auto_convert.lua
@@ -2,18 +2,17 @@ mputils = require 'mp.utils'
 
 function start_conversion_and_load_3d_audio()
    print("Switching over to 3d audio and restarting video!!");
-   local path = mp.get_property("path", "")
-   local dir, filename = mputils.split_path(path)
+   local file_path = mp.get_property("path", "")
    local curr_time_into = mp.get_property("time-pos","")
-   local sofa_file = "~/Videos/SurroundVideo/subject_119.sofa"
-   local command = "~/coding/dsvsca/DSVSCA/example/DSVSCA -v " .. path .. " -s " .. sofa_file  
+   local sofa_file = "~/Videos/SurroundVideos/subject_119.sofa"
+   local command = "~/coding/DSVSCA/example/DSVSCA -v " .. file_path .. " -s " .. sofa_file  
    -- local command = "~/coding/dsvsca/DSVSCA/example/DSVSCA -v " .. path .. " -s " .. sofa_file .. " &"
    -- Asynch version above, add a call to sleep(3 or so) to give it time to make header.
    os.execute(command)
-   local filename_to_open = filename:gsub("%.([%w%-]+)$", "") .. "-virtualized".. ".mp4"
+   local filename_to_open = file_path:gsub("%.([%w%-]+)$", "") .. "-virtualized".. ".mp4"
 
    local curr_time_into = mp.get_property("time-pos","")
-   mp.commandv("loadfile", dir..filename_to_open, "replace", "start=+"..curr_time_into) -- Remove curr_time_into if doing asynch
+   mp.commandv("loadfile", filename_to_open, "replace", "start=+"..curr_time_into) -- Remove curr_time_into if doing asynch
    mp.set_property_native("pause", false) 
 end
 
