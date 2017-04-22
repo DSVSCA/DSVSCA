b773ca6f4bc28382c347c574c45b1620a59f4f3b
Conor Landry <clandry94@ufl.edu>
2017-04-21 20:36:59 -0400

    Took care of dependencies for libva, llzma, and bzip2 using apt, which will be enough for now since we currently only support Ubuntu.

--- mpv/auto_convert.lua
+++ mpv/auto_convert.lua
@@ -4,8 +4,8 @@ function start_conversion_and_load_3d_audio()
    print("Switching over to 3d audio and restarting video!!");
    local file_path = mp.get_property("path", "")
    local curr_time_into = mp.get_property("time-pos","")
-   local sofa_file = "~/Videos/SurroundVideos/subject_119.sofa"
-   local command = "~/coding/DSVSCA/example/DSVSCA -v " .. file_path .. " -s " .. sofa_file  
+   local sofa_file = "conor.sofa"
+   local command = "./DSVSCA -v " .. file_path .. " -s " .. sofa_file  
    -- local command = "~/coding/dsvsca/DSVSCA/example/DSVSCA -v " .. path .. " -s " .. sofa_file .. " &"
    -- Asynch version above, add a call to sleep(3 or so) to give it time to make header.
    os.execute(command)
